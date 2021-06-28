0d84c53146242913057eb367570656cc2dc1263b
OtisWard4 <85902082+OtisWard4@users.noreply.github.com>
2021-06-28 14:48:14 -0500

    Adding Lab 1 & 2

---
+++ Lab2.py
@@ -0,0 +1,21 @@
+print()
+name = "LeBron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+
+
+grade = 11
+name = "Otis"
+intel_salary = 1100.50
+plays_videogames = True
+number_of_siblings = "3"
+
+
+
+print(type(grade))
+print(type(name))
+print(type(intel_salary))
+print(type(plays_videogames))
+print(type(number_of_siblings))
